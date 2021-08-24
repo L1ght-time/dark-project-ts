d98f4147f304579d333597bbd41ed2af3b4b9551
Maksym Konovalov <maksym.konovalov.cr@gmail.com>
2021-08-24 22:46:00 +0300

    DPT-003
    ### features
    
    -added basic pages and routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,20 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
 
-const App = () => <>My App</>;
+import Home from './pages/Home';
+import NotFound from './pages/NotFound';
+import Profile from './pages/Profile';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+
+const App = () => (
+  <>
+    <Route exact path="/" component={Home} />
+    <Route exact path="/not-found" component={NotFound} />
+    <Route exact path="/profile" component={Profile} />
+    <Route exact path="/sign-in" component={SignIn} />
+    <Route exact path="/sign-up" component={SignUp} />
+  </>
+);
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Switch } from 'react-router-dom';
+
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const app = (
   <React.StrictMode>
-    <App />
+    <Router>
+      <Switch>
+        <App />
+      </Switch>
+    </Router>
   </React.StrictMode>
 );
 
