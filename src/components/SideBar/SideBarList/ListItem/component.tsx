aacde43c929ab278b06cebdf80b4e95f9e4eef17
Nikita Lazurenko <nikita.lazurenko.d@gmail.com>
2022-02-22 21:08:32 +0200

    refactor: rebase ListItem component in sidebar section

--- src/components/SideBar/SideBarList/ListItem/component.tsx
+++ src/components/SideBar/SideBarList/ListItem/component.tsx
@@ -1,15 +1,15 @@
+import { Box, Typography } from '@mui/material';
 import React from 'react';
-import { ListItemButton } from '@mui/material';
 
 import MuiLink from '../../../shared/MuiLink';
 import { RoutesData } from '../../types';
 
 export function ListItem({ label, link }: RoutesData): JSX.Element {
   return (
-    <ListItemButton sx={{ pl: 4 }}>
-      <MuiLink to={link} underline='none'>
+    <Box component='li' sx={{ pl: 4, py: 1, display: 'block' }}>
+      <Typography component={MuiLink} to={link} sx={{ display: 'flex' }} underline='none'>
         {label}
-      </MuiLink>
-    </ListItemButton>
+      </Typography>
+    </Box>
   );
 }
