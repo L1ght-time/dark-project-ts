ff834c48a1e1719bd136eca307a1f937a4c58249
maksymkonovalov <maksym.konovalov@clockwise.software>
2022-03-11 21:37:31 +0200

    fix: added correct description types for FieldFormikText

--- src/components/formik/FieldFormikText/component.tsx
+++ src/components/formik/FieldFormikText/component.tsx
@@ -1,3 +1,11 @@
-export function FieldFormikText(): JSX.Element {
-  return <>FieldFormikText</>;
+import React from 'react';
+import { TextField, TextFieldProps } from '@mui/material';
+import { BaseTextFieldProps } from '@mui/material/TextField/TextField';
+
+interface IFieldFormikTextProps extends BaseTextFieldProps {
+  field: TextFieldProps;
+}
+
+export function FieldFormikText({ field, ...props }: IFieldFormikTextProps): JSX.Element {
+  return <TextField {...props} {...field} />;
 }
